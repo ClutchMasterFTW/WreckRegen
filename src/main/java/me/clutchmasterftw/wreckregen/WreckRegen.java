92971c2fbf92c01942471d3e556d7e0eb73b7d12
ClutchMasterFTW <clutchmasterftw@gmail.com>
2024-08-21 08:11:43 -0400

    Project Setup

---
+++ src/main/java/me/clutchmasterftw/wreckregen/WreckRegen.java
@@ -0,0 +1,17 @@
+package me.clutchmasterftw.wreckregen;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WreckRegen extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
