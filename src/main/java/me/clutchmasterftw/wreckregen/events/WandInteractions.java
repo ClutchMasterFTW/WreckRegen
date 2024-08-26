4c33c709ccbddecee902e5e3618e736b99cf446a
ClutchMasterFTW <69046374+ClutchMasterFTW@users.noreply.github.com>
2024-08-26 10:30:23 -0400

    Permission related quick-fix

--- src/main/java/me/clutchmasterftw/wreckregen/events/WandInteractions.java
+++ src/main/java/me/clutchmasterftw/wreckregen/events/WandInteractions.java
@@ -36,45 +36,47 @@ public class WandInteractions implements Listener {
 
         Player player = e.getPlayer();
 
-        if(data.has(new NamespacedKey(WreckRegen.getPlugin(), "isWreckRegenWand"), PersistentDataType.BOOLEAN) && player.hasPermission("wreckregen.admin")) {
-            Block block = e.getBlock();
-            Location location = block.getLocation();
-            int x = location.getBlockX();
-            int y = location.getBlockY();
-            int z = location.getBlockZ();
-            String blockType = block.getType().name();
+        if(data.has(new NamespacedKey(WreckRegen.getPlugin(), "isWreckRegenWand"), PersistentDataType.BOOLEAN)) {
+            if(player.hasPermission("wreckregen.admin")) {
+                Block block = e.getBlock();
+                Location location = block.getLocation();
+                int x = location.getBlockX();
+                int y = location.getBlockY();
+                int z = location.getBlockZ();
+                String blockType = block.getType().name();
 
-            player.sendMessage(WreckRegen.PREFIX + "You set the block at " + ChatColor.YELLOW + x + ", " + y + ", " + z + ChatColor.WHITE + " to regenerate as " + ChatColor.YELLOW + blockType + ChatColor.WHITE + ".");
+                player.sendMessage(WreckRegen.PREFIX + "You set the block at " + ChatColor.YELLOW + x + ", " + y + ", " + z + ChatColor.WHITE + " to regenerate as " + ChatColor.YELLOW + blockType + ChatColor.WHITE + ".");
 
-            e.setCancelled(true);
+                e.setCancelled(true);
 
-            List<Map<?, ?>> blocksToRegen = FILE.getMapList("blocks-to-regen");
-            for(int i = 0; i < blocksToRegen.size(); i++) {
-                int blockInfoX = (int) blocksToRegen.get(i).get("x");
-                int blockInfoY = (int) blocksToRegen.get(i).get("y");
-                int blockInfoZ = (int) blocksToRegen.get(i).get("z");
+                List<Map<?, ?>> blocksToRegen = FILE.getMapList("blocks-to-regen");
+                for(int i = 0; i < blocksToRegen.size(); i++) {
+                    int blockInfoX = (int) blocksToRegen.get(i).get("x");
+                    int blockInfoY = (int) blocksToRegen.get(i).get("y");
+                    int blockInfoZ = (int) blocksToRegen.get(i).get("z");
 //                String blockInfoType = (String) blocksToRegen.get(i).get("type");
 
-                if(x == blockInfoX && y == blockInfoY && z == blockInfoZ) {
-                    player.sendMessage(WreckRegen.PREFIX + "There is already a block set to regen at this position. Resetting its block type in the config to " + ChatColor.YELLOW + blockType + ".");
+                    if(x == blockInfoX && y == blockInfoY && z == blockInfoZ) {
+                        player.sendMessage(WreckRegen.PREFIX + "There is already a block set to regen at this position. Resetting its block type in the config to " + ChatColor.YELLOW + blockType + ".");
 
-                    //Remove the old version of the block at the location in the config
-                    blocksToRegen.remove(i);
-                    break;
+                        //Remove the old version of the block at the location in the config
+                        blocksToRegen.remove(i);
+                        break;
+                    }
                 }
-            }
-            //No matter what (even if the condition in the for loop above is true), insert the block Map back into the config.
-            Map<String, Object> blockMap = new HashMap<>();
-            blockMap.put("x", x);
-            blockMap.put("y", y);
-            blockMap.put("z", z);
-            blockMap.put("type", blockType);
+                //No matter what (even if the condition in the for loop above is true), insert the block Map back into the config.
+                Map<String, Object> blockMap = new HashMap<>();
+                blockMap.put("x", x);
+                blockMap.put("y", y);
+                blockMap.put("z", z);
+                blockMap.put("type", blockType);
 
-            blocksToRegen.add(blockMap);
-            FILE.set("blocks-to-regen", blocksToRegen);
-            WreckRegen.getPlugin().saveConfig();
-        } else if(!player.hasPermission("wreckregen.admin")) {
-            player.sendMessage(WreckRegen.PREFIX + ChatColor.RED + "You don't have permission to use this tool! Please contact an administrator for help if you believe this is incorrect.");
+                blocksToRegen.add(blockMap);
+                FILE.set("blocks-to-regen", blocksToRegen);
+                WreckRegen.getPlugin().saveConfig();
+            } else {
+                player.sendMessage(WreckRegen.PREFIX + ChatColor.RED + "You don't have permission to use this tool! Please contact an administrator for help if you believe this is incorrect.");
+            }
         }
     }
 
@@ -97,36 +99,38 @@ public class WandInteractions implements Listener {
         }
 
         PersistentDataContainer data = meta.getPersistentDataContainer();
-        if (data.has(new NamespacedKey(WreckRegen.getPlugin(), "isWreckRegenWand"), PersistentDataType.BOOLEAN) && player.hasPermission("wreckregen.admin")) {
-            if(player.isSneaking()) {
-                // Toggle client-side visualizations
-                ToggleVisualization.toggleVisuals(player.getUniqueId());
-            } else {
-                if(block == null) {
-                    return;
-                }
-                List<Map<?, ?>> blocksToRegen = FILE.getMapList("blocks-to-regen");
-                Location location = block.getLocation();
-                int x = location.getBlockX();
-                int y = location.getBlockY();
-                int z = location.getBlockZ();
-
-                int i = 0;
-                for (Map<?, ?> blockToRegen : blocksToRegen) {
-                    if ((int) blockToRegen.get("x") == x && (int) blockToRegen.get("y") == y && (int) blockToRegen.get("z") == z) {
-                        blocksToRegen.remove(i);
-                        FILE.set("blocks-to-regen", blocksToRegen);
-                        WreckRegen.getPlugin().saveConfig();
-                        player.sendMessage(WreckRegen.PREFIX + "Stopped the block at " + ChatColor.YELLOW + x + ", " + y + ", " + z + ChatColor.WHITE + " from regenerating.");
-
+        if (data.has(new NamespacedKey(WreckRegen.getPlugin(), "isWreckRegenWand"), PersistentDataType.BOOLEAN)) {
+            if(player.hasPermission("wreckregen.admin")) {
+                if(player.isSneaking()) {
+                    // Toggle client-side visualizations
+                    ToggleVisualization.toggleVisuals(player.getUniqueId());
+                } else {
+                    if(block == null) {
                         return;
                     }
-                    i++;
+                    List<Map<?, ?>> blocksToRegen = FILE.getMapList("blocks-to-regen");
+                    Location location = block.getLocation();
+                    int x = location.getBlockX();
+                    int y = location.getBlockY();
+                    int z = location.getBlockZ();
+
+                    int i = 0;
+                    for (Map<?, ?> blockToRegen : blocksToRegen) {
+                        if ((int) blockToRegen.get("x") == x && (int) blockToRegen.get("y") == y && (int) blockToRegen.get("z") == z) {
+                            blocksToRegen.remove(i);
+                            FILE.set("blocks-to-regen", blocksToRegen);
+                            WreckRegen.getPlugin().saveConfig();
+                            player.sendMessage(WreckRegen.PREFIX + "Stopped the block at " + ChatColor.YELLOW + x + ", " + y + ", " + z + ChatColor.WHITE + " from regenerating.");
+
+                            return;
+                        }
+                        i++;
+                    }
+                    player.sendMessage(WreckRegen.PREFIX + ChatColor.RED + "This block wasn't set to regenerate!");
                 }
-                player.sendMessage(WreckRegen.PREFIX + ChatColor.RED + "This block wasn't set to regenerate!");
+            } else {
+                player.sendMessage(WreckRegen.PREFIX + ChatColor.RED + "You don't have permission to use this tool! Please contact an administrator for help if you believe this is incorrect.");
             }
-        } else if (!player.hasPermission("wreckregen.admin")) {
-            player.sendMessage(WreckRegen.PREFIX + ChatColor.RED + "You don't have permission to use this tool! Please contact an administrator for help if you believe this is incorrect.");
         }
     }
 }
